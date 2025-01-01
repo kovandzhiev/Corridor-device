2441e0e7379bda6a1bcfb718efd54b09a5fd2f56
Plamen Kovandjiev <kovandjiev@gmail.com>
2025-01-01 19:29:22 +0200

    Use new way to work with FluentLight

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,25 @@
-// corridordevice.ino
-// Plamen Kovandzhiev's home projects
-// Web: https://github.com/kovandzhiev/Corridor-device
-// Supported boards:
-//		PRODINo WIFI-ESP WROOM-02 V1 (https://kmpelectronics.eu/products/prodino-wifi-esp-wroom-02-v1/)
-// Description:
-//		Manageing corridor lightings.
-// Used libraries:
-//      Fluent light (https://github.com/kovandzhiev/FluentLight)
-// Version: 1.0.0
-// Date: 26.12.2024
-// Author: Plamen Kovandzhiev <kovandjiev@gmail.com>
+/*!
+ * main.cpp
+ * This is firmware for Corridor device. This device will manage our corridor lights. The cases are varies:
+ * - switch on light 
+ *   - there are 2 PIR sensors mounted on the ceiling
+ *   - Dors - they have sensor which can receive signal for oper/close
+ * - manage light illumination by standard it is 100%
+ *   - no main power (UPS) reduce to 50%
+ *   - night - switch off 2 small lams, stays only 1 lamp. The illumination will br reduced to 30%
+ *
+ * Plamen Kovandzhiev's home projects
+ * Web: https://github.com/kovandzhiev/Corridor-device
+ * Supported boards:
+ * 		PRODINO WIFI-ESP WROOM-02 V1 (https://kmpelectronics.eu/products/prodino-wifi-esp-wroom-02-v1/)
+ * Description:
+ * 		Managing corridor lightings.
+ * Used libraries:
+ *      Fluent light (https://github.com/kovandzhiev/FluentLight)
+ * Version: 1.0.0
+ * Date: 01.01.2025
+ * Author: Plamen Kovandzhiev <kovandjiev@gmail.com>
+*/
 
 #include "KMPDinoWiFiESP.h"
 #include "FluentLight.h"
@@ -20,9 +30,9 @@
 #define NIGHT_BRIGHTNESS MAX_BRIGHTNESS / 3 // 30 % brightness
 #define LIGHT_RUNNING_DURATION_MS 1 * 60 * 1000 // 1 min
 #define LIGHT_BRIGHTEN_TIME_MS 5 * 1000 // 5 sec
-#define LIGHT_FADE_TIME_MS 1 * 60 * 1000 // 1 min
+#define LIGHT_FADE_TIME_MS 20 * 1000 // 20 sec
 
-FluentLight _lights(EXT_GROVE_D0);
+FluentLight _lights(EXT_GROVE_D0, MAX_BRIGHTNESS, DEFAULT_PWM_FREQUENCY);
 bool _isMotionDetected;
 bool _isMainPower;
 bool _isDay;
@@ -38,44 +48,47 @@ void processInputs() {
 }
 
 void processLogic() {
-	word brightness = MAX_BRIGHTNESS;
+	int brightness = MAX_BRIGHTNESS;
 
-	// If there is no main power, reduce brightness
+	// If there is no main power so the brightness is reduced to 50%
 	if (!_isMainPower)
 	{
 		brightness = NO_POWER_BRIGHTNESS;
 	}
 	
-	if (_isDay)
+	// If night the brightness is reduced to 30%
+	if (!_isDay)
 	{
-		/* code */
+		brightness = min(brightness, NIGHT_BRIGHTNESS);
 	}
+
+	// Turn Relay 1 to Off when Day include all lamps
+	//   or On when night exclude 2 small lamps
+	KMPDinoWiFiESP.SetRelayState(Relay1, !_isDay);
 	
+	_lights.setMaxBrightness(brightness);
 
-	_isMotionDetected = 
-		KMPDinoWiFiESP.GetOptoInState(OptoIn1) || // PIRs sensors
-		KMPDinoWiFiESP.GetOptoInState(OptoIn2); // Dors sensors
-
-	_isMainPower = KMPDinoWiFiESP.GetOptoInState(OptoIn3);
-
-	_isDay = KMPDinoWiFiESP.GetOptoInState(OptoIn4);
+	if (_isMotionDetected)
+	{
+		_lights.on();
+	}
 }
 
 void setup()
 {
+	delay(5000); // Prevent blocking upload of the new sketch
+
 #ifdef DEBUG
 	Serial.begin(115200);
 	Serial.println("Starting...");
 #endif
 
-	delay(5000); // Do not block upload of the new sketch
-
 	KMPDinoWiFiESP.init();
 
-	_lights.setMaxBrightness(MAX_BRIGHTNESS);
-	_lights.setRunningDuration(LIGHT_RUNNING_DURATION_MS);
 	_lights.setBrightenTime(LIGHT_BRIGHTEN_TIME_MS);
 	_lights.setFadeTime(LIGHT_FADE_TIME_MS);
+	_lights.setRunningDuration(LIGHT_RUNNING_DURATION_MS);
+	_lights.setMaxBrightness(MAX_BRIGHTNESS);
 
 	_lights.begin();
 
